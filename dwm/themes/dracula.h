db0f6cacfee4d7bfd96c4bce21edbbbe6cd7c5ef
Matt Weber <mtwb47@gmail.com>
2021-11-03 19:29:51 -0400

    Initial Init with new Monokai theme

---
+++ dwm/themes/dracula.h
@@ -0,0 +1,7 @@
+static const char col_gray1[]       = "#282a36";
+static const char col_gray2[]       = "#644475";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#282a36";
+static const char col_gray5[]       = "#a272a4";
+static const char col_cyan[]        = "#bf93f9";
+
